2d3934bdb30c078284693839898529e29b217157
Horia-Paul Ion <horiapaulion@gmail.com>
2020-02-15 18:57:49 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# Video player
+
+HTML5 video player with the YouTube player look and feel
+
+### Development guide
+
+```sh
+git clone https://github.com/ionhoria/video-player.git
+cd video-player
+npm install
+npm start
+```
---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const webpack = require('webpack');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader', 'eslint-loader'],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['*', '.js', '.jsx'],
+  },
+  plugins: [new webpack.HotModuleReplacementPlugin()],
+  devServer: {
+    contentBase: './dist',
+    hot: true,
+  },
+};
